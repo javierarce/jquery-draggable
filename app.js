29702ea4eba96f52dd71ed066c6c0e765fb86ca6
Javier Arce <javierarce@gmail.com>
2014-05-15 19:44:27 +0200

    Fixes names

--- app.js
+++ app.js
@@ -10,21 +10,21 @@ $(function() {
 
     self.allowDragging = true; 
 
-    var vertical_guides = [350];
+    var horizontal_guides = [350];
 
-    $canvas.append("<div class='line vertical one' />")
-    $canvas.find(".line.vertical.one").offset( { top: 350});
-    $canvas.find(".line.vertical.one").css( { left: 0 });
+    $canvas.append("<div class='guide horizontal one' />")
+    $canvas.find(".guide.horizontal.one").offset( { top: 350});
+    $canvas.find(".guide.horizontal.one").css( { left: 0 });
 
-    var horizontal_guides = [250, 500];
+    var vertical_guides = [250, 500];
 
-    $canvas.append("<div class='line horizontal one' />")
-    $canvas.find(".line.horizontal.one").offset( { left: 250});
-    $canvas.find(".line.horizontal.one").css( { top: 0 });
+    $canvas.append("<div class='guide vertical one' />")
+    $canvas.find(".guide.vertical.one").offset( { left: 250});
+    $canvas.find(".guide.vertical.one").css( { top: 0 });
 
-    $canvas.append("<div class='line horizontal two' />")
-    $canvas.find(".line.horizontal.two").offset( { left: 500});
-    $canvas.find(".line.horizontal.two").css( { top: 0 });
+    $canvas.append("<div class='guide vertical two' />")
+    $canvas.find(".guide.vertical.two").offset( { left: 500});
+    $canvas.find(".guide.vertical.two").css( { top: 0 });
 
     var onMouseDown = function(e) {
 
@@ -56,7 +56,7 @@ $(function() {
       var onMouseUp = function() {
         $(this).css('z-index', z_idx);
         self.allowDragging = false;
-        //$canvas.find(".line").remove();
+        //$canvas.find(".guide").remove();
       };
 
       var onMouseMove = function(e) {
@@ -72,9 +72,9 @@ $(function() {
         var canvas_right  = canvas_x + canvas_w;
         var canvas_bottom = canvas_y + canvas_h;
 
-        for (var i = 0; i < vertical_guides.length; i++) {
+        for (var i = 0; i < horizontal_guides.length; i++) {
 
-          var target_l = vertical_guides[i];
+          var target_l = horizontal_guides[i];
 
           if ( ( top >= target_l - opt.stickiness ) && ( top <= target_l + opt.stickiness ) ) {
             top = target_l;
@@ -84,9 +84,9 @@ $(function() {
             break;
           }
         }
-        for (var i = 0; i < horizontal_guides.length; i++) {
+        for (var i = 0; i < vertical_guides.length; i++) {
 
-          var target_l = horizontal_guides[i];;
+          var target_l = vertical_guides[i];
 
           if ( ( left >= target_l - opt.stickiness ) && ( left <= target_l + opt.stickiness ) ) {
             left = target_l;
